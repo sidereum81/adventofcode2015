1f4229a9be2ab6a2dce31d35b96255afbd9b3d03
Kenneth Karlsson <Kenneth Karlsson>
2017-02-10 22:56:12 +0100

    Add new files for adventOfCode2015

---
+++ src/main/java/org/adventofcode/adventofcode/DayOne.java
@@ -0,0 +1,5 @@
+package org.adventofcode.adventofcode;
+
+public class DayOne {
+
+}
---
+++ src/test/java/TestDayOne.java
@@ -0,0 +1,7 @@
+import org.testng.annotations.Test;
+
+public class TestDayOne {
+  @Test
+  public void f() {
+  }
+}
