f818a514707e3b2f9f0f092a3fcb9157cc2484cc
Kenneth Karlsson <Kenneth Karlsson>
2017-02-11 21:13:38 +0100

    Add the answer to Day One of adventOfCode and more testNG test

--- src/test/java/TestDayOne.java
+++ src/test/java/TestDayOne.java
@@ -13,6 +13,74 @@ public class TestDayOne {
 	}
 
 	@Test
-	public void testGetFloor() {
+	public void testGetFloor1() {
+		Double floorNumber = 0.0;
+		DayOne dayOne = new DayOne();
+		floorNumber = dayOne.getFloorNumber("()()()))");
+		Double expectedResult = -2.0;
+		Assert.assertEquals(floorNumber, expectedResult);
+	}
+
+	@Test
+	public void testGetFloor2() {
+		Double floorNumber = 0.0;
+		DayOne dayOne = new DayOne();
+		floorNumber = dayOne.getFloorNumber(")())())");
+		Double expectedResult = -3.0;
+		Assert.assertEquals(floorNumber, expectedResult);
+	}
+
+	@Test
+	public void testGetFloor3() {
+		Double floorNumber = 0.0;
+		DayOne dayOne = new DayOne();
+		floorNumber = dayOne.getFloorNumber(")))");
+		Double expectedResult = -3.0;
+		Assert.assertEquals(floorNumber, expectedResult);
+	}
+
+	@Test
+	public void testGetFloor4() {
+		Double floorNumber = 0.0;
+		DayOne dayOne = new DayOne();
+		floorNumber = dayOne.getFloorNumber("()()");
+		Double expectedResult = 0.0;
+		Assert.assertEquals(floorNumber, expectedResult);
+	}
+
+	@Test
+	public void testGetFloor5() {
+		Double floorNumber = 0.0;
+		DayOne dayOne = new DayOne();
+		floorNumber = dayOne.getFloorNumber("(())");
+		Double expectedResult = 0.0;
+		Assert.assertEquals(floorNumber, expectedResult);
+	}
+
+	@Test
+	public void testGetFloor6() {
+		Double floorNumber = 0.0;
+		DayOne dayOne = new DayOne();
+		floorNumber = dayOne.getFloorNumber("(()(()(");
+		Double expectedResult = 3.0;
+		Assert.assertEquals(floorNumber, expectedResult);
+	}
+
+	@Test
+	public void testGetFloor7() {
+		Double floorNumber = 0.0;
+		DayOne dayOne = new DayOne();
+		floorNumber = dayOne.getFloorNumber("(((");
+		Double expectedResult = 3.0;
+		Assert.assertEquals(floorNumber, expectedResult);
+	}
+
+	@Test
+	public void testGetFloor8() {
+		Double floorNumber = 0.0;
+		DayOne dayOne = new DayOne();
+		floorNumber = dayOne.getFloorNumber("))(((((");
+		Double expectedResult = 3.0;
+		Assert.assertEquals(floorNumber, expectedResult);
 	}
 }
