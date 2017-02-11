56c1493c9d6b3bd938de4cca1b833fa374dfd1f7
Kenneth Karlsson <Kenneth Karlsson>
2017-02-11 18:42:54 +0100

    Add second testNG test to dayone

--- src/test/java/TestDayOne.java
+++ src/test/java/TestDayOne.java
@@ -4,11 +4,15 @@ import org.testng.annotations.Test;
 
 public class TestDayOne {
 	@Test
-	public void testNegativeLevel() {
+	public void testCountCharacter() {
 		Integer negativeLevel;
 		DayOne countCharacter = new DayOne();
 		negativeLevel = countCharacter.countCharacters(')', "(())");
 		Integer expected = 2;
 		Assert.assertEquals(negativeLevel, expected);
+	}
+
+	@Test
+	public void testGetFloor() {
 	}
 }
