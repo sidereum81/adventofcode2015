dc2925787fe4dc027164f335cce28e1666a2a8b2
Kenneth Karlsson <Kenneth Karlsson>
2017-02-11 18:24:19 +0100

    Add first testNG test

--- src/main/java/org/adventofcode/adventofcode/DayOne.java
+++ src/main/java/org/adventofcode/adventofcode/DayOne.java
@@ -2,4 +2,13 @@ package org.adventofcode.adventofcode;
 
 public class DayOne {
 
+	public Integer countCharacters(char typeOfCharachterToCount, String stringToCountFrom) {
+		Integer numberOfMatches = 0;
+		for (int currentCharacter = 0; currentCharacter < stringToCountFrom.length(); currentCharacter++) {
+			if (stringToCountFrom.charAt(currentCharacter) == typeOfCharachterToCount) {
+				numberOfMatches++;
+			}
+		}
+		return numberOfMatches;
+	}
 }
--- src/test/java/TestDayOne.java
+++ src/test/java/TestDayOne.java
@@ -1,7 +1,14 @@
+import org.adventofcode.adventofcode.DayOne;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestDayOne {
-  @Test
-  public void f() {
-  }
+	@Test
+	public void testNegativeLevel() {
+		Integer negativeLevel;
+		DayOne countCharacter = new DayOne();
+		negativeLevel = countCharacter.countCharacters(')', "(())");
+		Integer expected = 2;
+		Assert.assertEquals(negativeLevel, expected);
+	}
 }
